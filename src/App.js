da0e132f7ffbd6a94b947175869da4a1a0b452ad
Shubham Goyal <ssgroup.net2022@gmail.com>
2022-06-26 15:53:57 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route /*,
+  Link*/
+} from "react-router-dom";
+
+import "./App.css";
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  let toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      showAlert("Dark Mode Enabled Successfully", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Light Mode Enabled Successfully", "success");
+    }
+  }
+
+  let showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Navbar title="Text App" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      <Routes>
+        <Route exact path="/about" element={<About />} />
+        <Route eaxct path="/" element={<TextForm showAlert={showAlert} title="Enter the Text to analyze" mode={mode} />} />
+      </Routes>
+
+    </Router>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+function TextForm(props) {
+  const [text, setText] = useState("Enter text here");
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+  function handleUpperClick() {
+    setText(text.toUpperCase());
+    props.showAlert("Successfully converted to Upper case!", "success");
+  }
+  function handleLowerClick() {
+    setText(text.toLowerCase());
+    props.showAlert("Successfully converted to Lower case!", "success");
+  }
+  function handleCapitalizeClick() {
+    const arr = text.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    let newText = arr.join(" ");
+    setText(newText);
+    props.showAlert("Successfully converted to Capitalize!", "success");
+  }
+  function handleClearClick() {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Successfully Cleared the text!", "success");
+  }
+
+  let numberOfWords = text.split(" ").filter((t)=> t!== "").length;
+
+  return (
+    <>
+      <div className="container my-3">
+        <h1>{props.title}</h1>
+        <textarea
+          className="form-control"
+          id="floatingTextarea"
+          rows={10}
+          value={text}
+          onChange={handleChange}
+          style={{
+            backgroundColor: props.mode === "light" ? "white" : "gray",
+            color: props.mode === "light" ? "black" : "white",
+          }}
+        ></textarea>
+        <button 
+          className="btn btn-primary my-3" 
+          onClick={handleUpperClick}
+          // style={{backgroundColor: props.mode === ('green' || 'blue') ? props.mode: '#0B5ED7'}}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          className="btn btn-primary my-3 mx-3"
+          onClick={handleLowerClick}
+          // style={{backgroundColor: props.mode === ('green' || 'blue') ? props.mode: '#0B5ED7'}}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          className="btn btn-primary my-3 mx-3"
+          onClick={handleCapitalizeClick}
+          // style={{backgroundColor: props.mode === ('green' || 'blue') ? props.mode: '#0B5ED7'}}
+        >
+          Convert to Capitalize
+        </button>
+        <button
+          className="btn btn-primary my-3 mx-3"
+          onClick={handleClearClick}
+          // style={{backgroundColor: props.mode === ('green' || 'blue') ? props.mode: '#0B5ED7'}}
+        >
+          Clear Text
+        </button>
+      </div>
+
+      <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>
+          {numberOfWords} words and {text.length} characters
+        </p>
+        <p>{0.008 * numberOfWords} minutes read</p>
+        <h2>Preview</h2>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter some text in the above textbox to get preview!"}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TextForm;
+
+TextForm.propTypes = {
+  title: PropTypes.string,
+  mode: PropTypes.string,
+};
